55c8b2fd051d13afbe7ec62f281f74d14d8cb1db
Cokely <JC032926@cerner.net>
2020-11-05 18:59:04 -0600

    Commiting all previous changes

---
+++ SpinningDiscs/BaseDisc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    public abstract class BaseDisc
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public Dictionary<int,string> Content { get; set; }
+        public string DiscType { get; set; }
+        public  int SpinRate { get; set; }
+
+        public BaseDisc(string name, int capacity, Dictionary<int,string> content, int spinRate)
+        {
+            Name = name;
+            Capacity = capacity;
+            Content = content;
+            DiscType = "";
+            SpinRate = 0;
+
+        }
+
+        public void UpdateName(string newName)
+        {
+            Name = newName;
+        }
+
+        public void UpdateCapacity(int newCapacity)
+        {
+            Capacity = newCapacity;
+
+        }
+
+        public void UpdateContent(Dictionary<int,string> newContent)
+        {
+            Content = newContent;
+        }
+
+        //public void UpdateContentType(string newDiscType)
+        //{
+        //    DiscType = newDiscType;
+        //}
+
+    }
+
+
+}
---
+++ SpinningDiscs/IOpticalDisc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    interface IOpticalDisc
+    {
+        void BeginSpinning();
+
+        void PauseSpinning();
+
+        void EndSpinning();
+
+        void SkipForward();
+
+        void SkipBackward();
+
+        void WriteData();
+
+        void ReadData();
+
+        void OutputData();
+
+    }
+}
